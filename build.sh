0ebea679488c9baa2e9c7fdb571f00e57fba754d
Carl Furrow <hello@carlfurrow.com>
2022-03-16 09:45:51 -0400

    Add more log output during build script
    may or may not be helpful

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 mkdir -p output/
+echo "Clearing out old build..."
+rm -rf output/*
 
 echo "Copy files to output folder..."
 cp alfred/info.plist.xml output/info.plist
@@ -7,12 +9,14 @@ cp alfred/calendar.png output/icon.png
 cp cal_to_md.swift output/
 
 curVersion=$(cat VERSION)
+echo "Current version: $curVersion"
 sed -i 's/{{version}}/'${curVersion}'/' output/info.plist
 
 readme='README.md'
+echo "Copy $readme into the info.plist..."
 sed -i -e "/{{readme}}/{r ${readme}" -e 'd' -e '}' output/info.plist
 
-echo "Zippig up version ${curVersion}..."
+echo "Zipping up version ${curVersion}..."
 cd output
 zip -Z deflate -rq9 Agenda.to.Markdown.alfredworkflow * -x etc
 cd -
